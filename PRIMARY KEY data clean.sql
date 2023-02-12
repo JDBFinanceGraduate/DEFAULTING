83ebdf6453139b70275d67d3ac2ae0c7131b18fe
JDBFinanceGraduate <124105570+JDBFinanceGraduate@users.noreply.github.com>
2023-02-12 16:40:03 -0600

    Update PRIMARY KEY data clean.sql

--- PRIMARY KEY data clean.sql
+++ PRIMARY KEY data clean.sql
@@ -1,3 +1,7 @@
 ALTER TABLE `fraud`.`default of credit card clients v2` 
 CHANGE COLUMN `ï»¿ID` `ID` INT NOT NULL ,
 ADD PRIMARY KEY (`ID`);
+/* 
+Changes the ID column in default of credit card clients v2 
+and adds primary key. 
+*\
