728991210881dc3f7b9964c51fe79a0d548cbc95
JDBFinanceGraduate <124105570+JDBFinanceGraduate@users.noreply.github.com>
2023-02-12 16:36:34 -0600

    Add files via upload

---
+++ PRIMARY KEY data clean.sql
@@ -0,0 +1,3 @@
+ALTER TABLE `fraud`.`default of credit card clients v2` 
+CHANGE COLUMN `ï»¿ID` `ID` INT NOT NULL ,
+ADD PRIMARY KEY (`ID`);
---
+++ text data clean.sql
@@ -0,0 +1,16 @@
+UPDATE default_of_credit_card_clients_v2
+SET SEX = CASE 
+    WHEN SEX = 1 THEN 'male'
+    WHEN SEX = 2 THEN 'female'
+END,
+EDUCATION = CASE 
+    WHEN EDUCATION = 1 THEN 'graduate school'
+    WHEN EDUCATION = 2 THEN 'university'
+    WHEN EDUCATION = 3 THEN 'high school'
+    WHEN EDUCATION = 4 THEN 'others'
+END,
+MARRIAGE = CASE 
+    WHEN MARRIAGE = 1 THEN 'married'
+    WHEN MARRIAGE = 2 THEN 'single'
+    WHEN MARRIAGE = 3 THEN 'others'
+END;
